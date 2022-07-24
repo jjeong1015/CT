a9e204a50cf82e18bb195ae1defb64ab2163488e
이정민 <82391356+jjeong1015@users.noreply.github.com>
2022-07-24 14:16:34 +0900

    Update P17298.java

--- src/DataStructure/P17298.java
+++ src/DataStructure/P17298.java
@@ -12,7 +12,6 @@ public class P17298 {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int A[] = new int[N];
 		int ans[] = new int[N];
